394220d5c14b0e8756463cfc4c88dd5f6d84a73e
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-04 14:52:17 +0900

    UserController修正&演習４完了

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.domain.User;
+import com.example.form.UserForm;
 
 /**
  * 
@@ -14,16 +19,27 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping("/exam04")
 public class Exam04Controller {
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam04";
 	}
 	
 	@RequestMapping("post")
-	public String post(/*@Validated userForm, 
-			BindingResult result,*/
-			RedirectAttributes redirectAttributes,
-			Model model) {
+	public String post(@Validated UserForm form, 
+			BindingResult result,
+			RedirectAttributes redirectAttributes) {
+		if (result.hasErrors()) {
+			return index();
+		}
+		
+		User user = new User();
+		BeanUtils.copyProperties(form, user);
+		redirectAttributes.addFlashAttribute("user", user);
 		
 		return "redirect:/exam04/show";
 	}
--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -1,14 +1,13 @@
 package com.example.form;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserForm {
 	@NotBlank(message = "名前は必須です")
 	private String name;
-	@NotBlank(message = "年齢は必須です")
-	@Pattern(regexp = "^[0-9]+$", message = "数字で入力してください")
+	@NotNull(message = "年齢は必須です")
 	private Integer age;
 	@Size(min = 1, max = 127, message = "コメントは1文字以上127文字以内で記載してください")
 	private String comment;
