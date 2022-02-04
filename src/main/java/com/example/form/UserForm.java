9a4409c5d3d30782a1f91cc892191d68c92cb138
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-04 16:02:56 +0900

    ageに文字が入らないよう修正

--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -1,14 +1,15 @@
 package com.example.form;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class UserForm {
 	@NotBlank(message = "名前は必須です")
 	private String name;
-	@NotNull(message = "年齢は必須です")
-	private Integer age;
+	@NotBlank(message = "年齢は必須です")
+	@Pattern(regexp = "^[0-9]+$", message = "値を入力してください")
+	private String age;
 	@Size(min = 1, max = 127, message = "コメントは1文字以上127文字以内で記載してください")
 	private String comment;
 	
@@ -18,10 +19,10 @@ public class UserForm {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getAge() {
+	public String getAge() {
 		return age;
 	}
-	public void setAge(Integer age) {
+	public void setAge(String age) {
 		this.age = age;
 	}
 	public String getComment() {
